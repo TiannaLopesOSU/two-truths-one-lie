78271eae54ba33ef4156a1ec8c16319c93650f75
TiannaLopesOSU <lopesti@oregonstate.edu>
2023-05-11 12:51:25 -0700

    Use button instead of div

--- two-truths-one-lie/src/components/QuizView.vue
+++ two-truths-one-lie/src/components/QuizView.vue
@@ -15,7 +15,8 @@
   <!-- Facts -->
   <div class="container">
     <div class="card m-5 w-75" v-for="fact in getFacts" :key="fact.uniqueId">
-      <div
+      <!-- use button instead -->
+      <button
         class="card-body p-"
         @click="handleSubmit(fact)"
         :class="{
@@ -62,7 +63,7 @@
             d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"
           />
         </svg>
-      </div>
+      </button>
     </div>
   </div>
 
@@ -148,6 +149,7 @@ export default {
       }
     },
     handleSubmit(answer) {
+      // create audio once only (ex: mounted)
       if (answer.truth === false) {
         const correct = new Audio(require("../assets/correct.mp3"));
         correct.play();
